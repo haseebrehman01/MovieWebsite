c6b7be2c17acba86be981acc9cb73bfb60d46531
Haseeb ur rehman <125881790+haseebrehman01@users.noreply.github.com>
2025-09-13 22:23:01 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Movie from './Movie'
+import Contact from './Contact'
+import AppLayout from './Components/Layout/AppLayout'
+function App() {
+const router = createBrowserRouter([
+ {
+  path:'/',
+  element:<AppLayout />,
+  children:[
+    {
+    path: '/',
+    element: <Home />
+  },
+    {
+    path: '/About',
+    element: <About />
+  },
+      {
+    path: '/Movie',
+    element: <Movie />
+  },
+      {
+    path: '/Contact',
+    element: <Contact />
+  },
+]
+
+ }
+])
+
+return <RouterProvider router={router} />
+}
+
+export default App
